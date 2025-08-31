cdb206086331df11bf80779ee3da1e0badd86153
Nayeb Ahmed Qureshi <nayebqureshi4@gmail.com>
2025-09-01 00:04:17 +0600

    done with open source chat models using hugging face api

--- ChatModels/chatmodelopenai.py
+++ ChatModels/chatmodelopenai.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-api_key = os.getenv("OPEN_AI_Key")
+# api_key = os.getenv("OPEN_AI_Key")
 
-model=ChatOpenAI(model='gpt-4',temperature=0.7,max_completion_tokens=10, api_key=api_key)
+model=ChatOpenAI(model='gpt-4',temperature=0.7,max_completion_tokens=100)
 
 result=model.invoke("What is the capital of France?")
 
--- LLMS/llm_demo.py
+++ LLMS/llm_demo.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-api_key = os.getenv("OPEN_AI_Key")
 
-llm= OpenAI(model='gpt-3.5-turbo-instruct', api_key=api_key)
+
+llm= OpenAI(model='gpt-3.5-turbo-instruct')
 
 result=llm.invoke("What is the capital of France?")
 
